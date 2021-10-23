20eccf187c9d7bee7f2d0e230faab36fdb088c71
fantasy-q <Kozmos941@gmail.com>
2021-10-23 11:06:55 +0800

    创建项目

---
+++ docs/_coverpage.md
@@ -0,0 +1,8 @@
+<!-- _coverpage.md -->
+
+<img src="_media/pattern.svg" alt="logo" style="zoom:50%;" />
+
+# 提瓦特大学出版社
+
+> [伤害乘区论](伤害乘区论.md)
+> [高等元素论](高等元素论.md)
---
+++ docs/docsify.config.js
@@ -0,0 +1,8 @@
+window.$docsify = {
+  // name: '提瓦特大学',
+  repo: 'Kozmos941/teyvat-university-press',
+  coverpage: true,
+  onlyCover: true,
+  // plugins,
+}
+
